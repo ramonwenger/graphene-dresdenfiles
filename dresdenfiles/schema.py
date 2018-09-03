3626b907d77432b0429522cae61e1348a3562fd7
Ramon Wenger <donramonloco+github@gmail.com>
2018-09-03 11:30:37 +0200

    Add mutations to schema

--- dresdenfiles/schema.py
+++ dresdenfiles/schema.py
@@ -3,9 +3,12 @@ import graphene
 import wizards.schema
 from graphene_django.debug import DjangoDebug
 
+from wizards.schema import Mutation
+
+
 class Query(wizards.schema.Query,
             graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name='__debug')
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- wizards/schema.py
+++ wizards/schema.py
@@ -1,41 +1,102 @@
+from django.core.exceptions import ValidationError
 from graphene_django import DjangoObjectType
 import graphene
+from graphene import Node, relay, InputObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from graphql_relay import from_global_id
 
 from .models import Wizard, Place, Spell
 
 
-class WizardType(DjangoObjectType):
+class WizardNode(DjangoObjectType):
     class Meta:
         model = Wizard
+        interfaces = (Node,)
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'school': ['exact']
+        }
 
 
-class PlaceType(DjangoObjectType):
+class PlaceNode(DjangoObjectType):
     class Meta:
         model = Place
+        interfaces = (Node,)
+        filter_fields = ['name']
 
 
-class SpellType(DjangoObjectType):
+class SpellNode(DjangoObjectType):
     class Meta:
         model = Spell
+        interfaces = (Node,)
+        filter_fields = ['name']
+
+
+class CreateWizardInput(InputObjectType):
+    name = graphene.String(required=True)
+    school = graphene.String(required=True)
+
+
+
+class AddWizard(relay.ClientIDMutation):
+    class Input:
+        wizard = graphene.Argument(CreateWizardInput)
+
+    new_wizard = graphene.Field(WizardNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **args):
+        wizard_data = args.get('wizard')
+        new_wizard = Wizard.objects.create(**wizard_data)
+        return cls(new_wizard=new_wizard)
+
+
+class UpdateWizard(relay.ClientIDMutation):
+    class Input:
+        wizard = graphene.Argument(CreateWizardInput)
+        id = graphene.String(required=True)
+
+    errors = graphene.List(graphene.String)
+    updated_wizard = graphene.Field(WizardNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **args):
+        try:
+            id = from_global_id(args.get('id'))[1]
+            wizard = Wizard.objects.get(pk=id)
+            if wizard:
+                wizard_data = args.get('wizard')
+                for key, value in wizard_data.items():
+                    setattr(wizard, key, value)
+                return cls(updated_wizard=wizard)
+        except ValidationError as e:
+            return cls(updated_wizard=None, errors=e)
+
+
+
+class Mutation(graphene.ObjectType):
+    add_wizard = AddWizard.Field()
+    update_wizard = UpdateWizard.Field()
+
 
 class Query(object):
-    wizard = graphene.Field(WizardType, id=graphene.Int(), name=graphene.String())
-    wizards = graphene.List(WizardType)
-    places = graphene.List(PlaceType)
-    spells = graphene.List(SpellType)
+    wizard = Node.Field(WizardNode)
+    wizards = DjangoFilterConnectionField(WizardNode)
+    places = DjangoFilterConnectionField(PlaceNode)
+    spells = DjangoFilterConnectionField(SpellNode)
 
-    def resolve_wizard(self, info, *args, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
+    # def resolve_wizard(self, info, *args, **kwargs):
+    #     id = kwargs.get('id')
+    #     name = kwargs.get('name')
+    #
+    #     if id is not None:
+    #         return Wizard.objects.prefetch_related('known_spells').get(pk=id)
+    #     if name is not None:
+    #         return Wizard.objects.prefetch_related('known_spells').get(name=name)
+    #
+    #     return None
 
-        if id is not None:
-            return Wizard.objects.prefetch_related('known_spells').get(pk=id)
-        if name is not None:
-            return Wizard.objects.prefetch_related('known_spells').get(name=name)
-
-        return None
-
-    def resolve_wizards(self, info):
+    def resolve_wizards(self, info, *args, **kwargs):
         return Wizard.objects.all()
 
     def resolve_places(self, info):
